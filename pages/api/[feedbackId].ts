e5bb80ec49eab527362de8ac2720689fe5a65510
TranManhThang <tranmanhthang96@gmail.com>
2024-01-09 23:36:19 +0700

    dynamic api routes

---
+++ pages/api/[feedbackId].ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getData, getFilePath } from './feedback';
+
+type Data = {
+  feedback: any;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const feedbackId = req.query.feedbackId;
+  const filePath = getFilePath();
+  const data: any[] = getData(filePath);
+  const selectedFeedback = data.find((item) => (item.id = feedbackId));
+  res.status(200).json({ feedback: selectedFeedback });
+}
--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -1,13 +1,31 @@
 import { getFilePath, getData } from '../api/feedback';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Feedback = ({ feedbacks }: { feedbacks: any[] }) => {
+  const [currentFeedback, setCurrentFeedback] = useState<any>(null);
+
+  const loadFeedbackHandler = (id: string) => {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCurrentFeedback(data.feedback);
+      })
+      .catch((error) => console.log(error));
+  };
   return (
-    <ul>
-      {feedbacks.map((item) => (
-        <li key={item.id}>{item.feedback}</li>
-      ))}
-    </ul>
+    <>
+      {currentFeedback && <p>{currentFeedback.feedback}</p>}
+      <ul>
+        {feedbacks.map((item) => (
+          <li key={item.id}>
+            {item.feedback}{' '}
+            <button onClick={() => loadFeedbackHandler(item.id)}>
+              Show Detail
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
