109de60b359cec898ce595f4254ee80fecabe7ae
TranManhThang <tranmanhthang96@gmail.com>
2024-01-14 12:31:44 +0700

    render code snippets

--- components/PostDetail/PostContent.tsx
+++ components/PostDetail/PostContent.tsx
@@ -3,13 +3,53 @@ import PostHeader from './PostHeader';
 import classes from './styles/post-content.module.css';
 import ReactMarkdown from 'react-markdown';
 import { IPost } from '@/types';
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark as syntaxStyle } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const PostContent = ({ post }: { post: IPost }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          img(props) {
+            const { node, ...rest } = props;
+            if (rest.src && rest.alt) {
+              return (
+                <Image
+                  src={rest.src}
+                  alt={rest.alt}
+                  width={600}
+                  height={300}
+                  layout="responsive"
+                />
+              );
+            }
+          },
+          code(props) {
+            const { children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || '');
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                language={match[1]}
+                style={syntaxStyle}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 };
--- data/posts/getting-started-with-nextjs.md
+++ data/posts/getting-started-with-nextjs.md
@@ -6,6 +6,38 @@ excerpt: 'Optimized compute. Deliver globally performant apps without additional
 isFeatured: true
 ---
 
-# This is a title
+NextJS is a **framework for ReactJS**.
 
-This is some regular text with a [link](https://github.com/thangtm96)
+Wait a second ... a "framework" for React? Isn't React itself already a framework for JavaScript?
+
+Well ... first of all, React is a "library" for JavaScript. That seems to be important for some people.
+
+Not for me, but still, there is a valid point: React already is a framework / library for JavaScript. So it's already an extra layer on top of JS.
+
+## Why would we then need NextJS?
+
+Because NextJS makes building React apps easier - especially React apps that should have server-side rendering (though it does way more than just take care of that).
+
+In this article, we'll dive into the core concepts and features NextJS has to offer:
+
+- File-based Routing
+- Built-in Page Pre-rendering
+- Rich Data Fetching Capabilities
+- Image Optimization
+- Much More
+
+```javascript
+const a = 1;
+const b = 2;
+
+function sum(a, b) {
+  return a + b;
+}
+sum(a, b)
+```
+
+## File-based Routing
+
+![Create routes via your file + folder structure](/images/posts/getting-started-with-nextjs/getting-started-nextjs.png)
+
+... More content ...
